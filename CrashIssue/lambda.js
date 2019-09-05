48e696d4a86d4263c9ef2c50852608b300c7a2a8
Indunil1992 <indunil@adroitlogic.com>
2019-09-05 11:01:17 +0530

    try to commit : indunil

--- CrashIssue/lambda.js
+++ CrashIssue/lambda.js
@@ -1,4 +1,4 @@
 exports.handler = function (event, context, callback) {
 
-    callback(null, { "message": "Successfully executed indu" });
+    callback(null, { "message": "Successfully executed indunil" });
 }
